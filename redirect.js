e8126fa5244cb2dd42917ef4680d8f7f363fb62a
mtsferreirasilva <mtsferreirasilva@gmail.com>
2017-04-25 23:38:17 -0400

    Updating URL

--- redirect.js
+++ redirect.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
       var url = tabs[0].url;
-      var readerUrl = 'http://contentify.herokuapp.com/reader?url=';
+      var readerUrl = 'https://www.contentify.io/reader?url=';
 
       chrome.tabs.update({url: readerUrl + url});
   });
